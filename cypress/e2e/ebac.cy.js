13171dbb18f9a0fc90c42a0f0fa7d23087f7a38e
Bryan Souza <bryan.soares19@hotmail.com>
2023-03-25 18:46:02 -0300

    rest

---
+++ cypress/e2e/ebac.cy.js
@@ -0,0 +1,104 @@
+/// <reference types="Cypress" />
+import { faker } from '@faker-js/faker';
+
+
+context('Testes Automatizados de API REST ', () => {
+    let authorization;
+    let id;
+    beforeEach('login', () => {
+        cy.request({
+            method:'POST', 
+            url: 'http://localhost:3000/login', 
+            body: {
+                    "email":"fulano@qa.com",
+                    "password":"teste"
+                  }
+            }).then((response) => {
+                
+            authorization = response.body.authorization
+            expect(response.status).to.eq(200); 
+            expect(response.body.message).to.equal('Login realizado com sucesso');
+          });
+    });
+
+
+    describe('Automação GET - POST', () => {
+       
+        describe('Exercicio 1', () => {
+            
+        // Escreva um teste que verifique se um produto pode ser criado com sucesso,
+        // e em seguida, faça uma solicitação GET para verificar se o produto 
+        // está na lista de produtos retornada.
+
+            it('Adicionando produto', () => {
+                cy.request({
+                    method: "POST",
+                    url: "http://localhost:3000/produtos",
+                    body: {
+                            "nome": `${faker.commerce.productName()}`,
+                            "preco": `${Math.abs(Math.round(Math.random() * 10))}`,
+                            "descricao": `${faker.commerce.productDescription()}`,
+                            "quantidade": `${Math.abs(Math.round(Math.random() * 10))}`
+                    },
+                    headers: { authorization: authorization }
+                    
+                }).then((response) =>{
+                    id = response.body._id
+                })
+            })
+            it('Listando o produto adicionado', () => {
+                    cy.request({
+                        method: "GET",
+                        url: `http://localhost:3000/produtos/${id}`
+                    })
+               
+               
+            });
+        });
+        describe('Exercicio 2', ()=>{
+
+            // Crie um teste que verifique se o tempo de resposta de uma 
+            // solicitação GET é menor do que um determinado valor.
+
+            it('Verificação do tempo de resposta menor que 500', () => {
+                cy.request({
+                    method: "GET",
+                    url: "http://localhost:3000/produtos",
+                }).its('duration')
+                .should('be.lessThan', 500)
+            });
+        });
+        describe('Exercicio 3', () =>{
+            
+            // Escreva um teste que verifique se um produto pode ser editado com sucesso e, 
+            // em seguida, faça uma solicitação GET para verificar se as informações 
+            // do produto foram atualizadas corretamente.
+
+            it('Editar um produto', () => {
+                it('', () => {
+                    
+                });
+            });
+            it('Verificar se o produto foi realmente editado', () => {
+                cy.request({
+                    method: "PUT",
+                    url: `http://localhost:3000/produtos/${id}`,
+                    body: {
+                        "nome": `${faker.commerce.productName()}`,
+                        "preco": `${Math.abs(Math.round(Math.random() * 10))}`,
+                        "descricao": `${faker.commerce.productDescription()}`,
+                        "quantidade": `${Math.abs(Math.round(Math.random() * 10))}`
+                    },
+                    headers: { authorization: authorization}
+                }).then((response) => {
+                    expect(response.body.message).to.equal("Registro alterado com sucesso")
+                })
+            });
+        })
+
+
+});
+
+
+
+})// end context
